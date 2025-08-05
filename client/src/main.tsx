f0aec24f57abf386fe7cb3d2f88ca5ff3e5bcb54
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2025-08-05 13:08:40 +0200

    feat: added MUI and react-redux to client dependencies

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { CssBaseline } from '@mui/material'
+
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+      <CssBaseline />
+      <App />
+  </StrictMode>
 )
